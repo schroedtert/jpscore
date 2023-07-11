a1c47ade8e34c387226427bb43cfe47d30235fcc
schroedtert <t.schroedter@fz-juelich.de>
2023-07-11 14:06:27 +0200

    Add scripts to format python files with black and isort

---
+++ scripts/formatting/format-py.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+location="$(cd "$(dirname "${0}")";pwd -P)"
+root="$(cd "$(dirname "${location}/../../..")";pwd -P)"
+
+njobs=1
+if [[ "$(uname)" == "Darwin" ]]; then
+    njobs=$(sysctl -n hw.logicalcpu)
+elif [[ "$(uname)" == "Darwin" ]]; then
+    njobs=$(nproc)
+fi
+
+isort --jobs "${njobs}" "${root}"
+black "${root}"
